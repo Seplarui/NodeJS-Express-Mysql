cb7c71026b2abb46a3aafc45a8b3cd7e8d41e6a7
Sebas <seplarui@gmail.com>
2017-04-23 14:05:14 +0200

    Añadido ruta de borrado

--- routes/users.js
+++ routes/users.js
@@ -40,13 +40,40 @@ router.post('/new', function (req, res) {
     connection.query('insert into usuario set ?', nuevo_usuario, function (err, success) {
         if (err) throw err;
         console.log('Datos grabados', success.insertId);
-        console.log("Cuerpo:"+req.body);
+        console.log("Cuerpo:" + req.body);
         console.log("Paramétros:" + req.params);
         console.log(cifrado);
-
-    })
-
-
     });
+    /*Redirige a index*/
+    res.redirect('/');
+});
+/****MOSTRAR UN USUARIO POR ID*****/
+router.get('/:id', function (req, res) {
+    var userId = req.params.id;
+    connection.query('select * from usuario where id=' + userId, function (err, results) {
+        if (err) throw err;
+        console.log(userId);
+        res.status(200).send(results);
+        console.log(results);
+    });
+});
+
+/*******ELIMINAR USUARIO*******/
+
+router.post('/delete/:id', function (req, res) {
+    var userId = req.params.id;
+
+    connection.query('delete from usuario where id=' + userId, function (err, results) {
+        if (err) throw err;
+        console.log("Registro borrado: "+userId);
+        console.log(results);
+    });
+    /*Redirige a index*/
+    res.redirect('/');
+});
+
+
+
+
 
 module.exports = router;

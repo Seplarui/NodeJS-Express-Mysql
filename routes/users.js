dfb93626888ea65a17f2ed405701dee06e4a267c
Sebas <seplarui@gmail.com>
2017-04-23 12:44:49 +0200

    Cifrado password

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var mysql=require('mysql')
+var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
 //***CONEXIÓN A LA BASE DE DATOS****//
 
 
@@ -8,12 +9,8 @@ var dbconfig = require('./../config/database');
 var connection = mysql.createConnection(dbconfig.connection);
 
 connection.query('USE ' + dbconfig.database);
-/* GET users listing. */
-/*router.get('/', function(req, res) {
 
-
-  res.send('respond with a resource');
-});*/
+/*****LISTADO DE USUARIOS****/
 
 router.get('/', function (req, res) {
     connection.query("select * from usuario", function (err, results) {
@@ -25,51 +22,27 @@ router.get('/', function (req, res) {
         //return next();
     })
     //res.send('Aqui va el listado de usuarios');
-
-    
-
-
-    
    
 });
 
+/*******AÑADIR NUEVO USUARIO******/
+
 router.post('/new', function (req, res) {
     //res.send('prueba new');
+    var cifrado = bcrypt.hashSync(req.body.password, null, null);
 
     var usuario = {};
-    usuario.usuario = req.params.usuario;
-    usuario.nombre = req.params.nombre;
-    usuario.password = req.params.password;
-
-    /*connection.query('insert into usuario(usuario, password,nombre) values (\'' + usuario.usuario + '\',\'' + usuario.password + '\',\'' + usuario.nombre + '\',\')', function (err, success) {
-        if (err) throw err;
-        res.send(200, success.Id);
-    })*/
-
-   /* connection.query('insert into usuario(usuario,password,nombre)' + 'values(' + usuario.usuario + ',' + usuario.password + ',' + usuario.nombre + ')', function (err, sucess) {
-        if (err) throw err;
-        res.send(200, succes.id);
-    })*/
-    /**
-     * FUNCIONA
-     */
-    /*var nuevo_usuario = { usuario: 'usuario3', password: 'password3', nombre: 'nombre3' };
-    connection.query('insert into usuario set ?', nuevo_usuario, function (err, res) {
-        if (err) throw err;
-        console.log('Datos Grabados', res.id);
-    });*/
-
-    var nuevo_usuario = { usuario: '\'\'' + usuario.usuario + '\'', password: '\'\'' + usuario.password + '\'', nombre: '\'\'' + usuario.nombre + '\'' }
+    usuario.usuario = req.body.usuario;
+    usuario.nombre = req.body.nombre;
+    usuario.password = cifrado;
+    var nuevo_usuario = { usuario: usuario.usuario, password: usuario.password, nombre: usuario.nombre };
+    //var nuevo_usuario = { usuario: '\'\'' + usuario.usuario + '\'', password: '\'\'' + cifrado + '\'', nombre: '\'\'' + usuario.nombre + '\'' }
     connection.query('insert into usuario set ?', nuevo_usuario, function (err, success) {
         if (err) throw err;
         console.log('Datos grabados', success.insertId);
-        console.log(req.body);
-        //res.send(200, success.insertId);
-        //res.status(200).send();
-        res.status(200).send(req.params);
-        console.log("Este es el nuevo usuario introducido: " + nuevo_usuario);
-
-       // res.status(200).send('Usuario añadido a la BBDD de manera correcta: ' + res.insertId);
+        console.log("Cuerpo:"+req.body);
+        console.log("Paramétros:" + req.params);
+        console.log(cifrado);
 
     })
 
